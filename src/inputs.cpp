7040aa5cd385de31e9ff2a368ae27011fe54c42d
Pablo Ibarz <63760047+Roboticy3@users.noreply.github.com>
2023-06-26 16:22:37 -0400

    Renamed inputs to buttons

--- src/inputs.cpp
+++ src/inputs.cpp
@@ -6,9 +6,8 @@
 #include "inputs.h"
 #include <GLFW/glfw3.h>
 
-const char* unknown_key = (char*)"?name?";
-
-enum inputs {
+/* Definitions for each input that can be used on this application */
+enum INPUT_BUTTONS {
     INPUTS_CONFIRM = 0,
     INPUTS_ESCAPE,
 
@@ -20,10 +19,18 @@ enum inputs {
     INPUTS_M_UP,
     INPUTS_M_DOWN,
     INPUTS_M_RIGHT,
-    INPUTS_M_LEFT
+    INPUTS_M_LEFT,
+
+    /* keep this enum to count the number of terms in inputs */
+    INPUTS_NUM
 };
 
-const std::unordered_map<int, inputs> input_map {
+/* Keep track of which buttons are currently pressed */
+bool button_mask[INPUTS_NUM] = {0};
+
+/* Mapping that allows for keys from GLFW to be assigned to inputs
+    Multiple keys can be mapped to the same input, but each key can only represent one input*/
+const std::unordered_map<int, INPUT_BUTTONS> button_map {
     
     {GLFW_KEY_SPACE, INPUTS_CONFIRM},
     {GLFW_KEY_ENTER, INPUTS_CONFIRM},
@@ -31,23 +38,28 @@ const std::unordered_map<int, inputs> input_map {
     {GLFW_KEY_ESCAPE, INPUTS_ESCAPE},
     
     {GLFW_KEY_UP, INPUTS_UP},
-    {GLFW_KEY_DOWN, INPUTS_M_DOWN},
-    {GLFW_KEY_RIGHT, INPUTS_M_RIGHT},
-    {GLFW_KEY_LEFT, INPUTS_M_LEFT},
+    {GLFW_KEY_DOWN, INPUTS_DOWN},
+    {GLFW_KEY_RIGHT, INPUTS_RIGHT},
+    {GLFW_KEY_LEFT, INPUTS_LEFT},
+
+    {GLFW_KEY_W, INPUTS_M_UP},
+    {GLFW_KEY_S, INPUTS_M_DOWN},
+    {GLFW_KEY_D, INPUTS_M_RIGHT},
+    {GLFW_KEY_A, INPUTS_M_LEFT},
     
 };
 
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods) {
 
-    auto itr = input_map.find(key);
-    if (itr == input_map.end()) {
-        std::cout << "undefined input " << key << "\n";
+    auto itr = button_map.find(key);
+    if (itr == button_map.end()) {
+        //std::cout << "undefined input " << key << "\n";
         return;
     }
 
     auto[k,i] = *itr;
 
-    std::cout << "input " << i << "\n";
+    button_mask[i] = action;
 }
 
 void init_inputs(GLFWwindow* window) {
